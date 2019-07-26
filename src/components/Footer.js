eeec7d4378eaf113200728f7764e75cd76bfaa67
amitp <amitpatel_7@msn.com>
2019-07-25 17:55:01 -0700

    added elements to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,27 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer>
+        <div className="footer-container">
+          <div className="contact">
+            <h4>Cellar Door Help Line</h4>
+            <h5>559-239-4004</h5>
+          </div>
+          <Link to="#">
+            <img src="img/footer_logo.png" alt="cellar door"/>
+          </Link>
+          <div className="social-media">
+            <div className="social-icon-container"></div>
+
+
+
+          </div>
+
+
+
+
+
+
+        </div>
       </footer>
     )
   }
