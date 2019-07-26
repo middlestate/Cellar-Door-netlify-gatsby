551accde7e6eb30c298a22283cfddd07a88227b8
amitp <amitpatel_7@msn.com>
2019-07-25 17:33:22 -0700

    completed IndexPageTemplate elements

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,20 +1,74 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 
 export const IndexPageTemplate = ({
   title,
-  description
+  description,
+  events_button_title
 }) => (
-  <div>
-    <h2>{title}</h2>
-    <p>{description}</p>
-  </div>
+  <main>
+    <div className="title-container">
+      <img src="img/logo.png" alt="cellar door" />
+    </div>
+
+    <div className="upcoming-events">
+      <div className="header">
+        <h3>{events_button_title}</h3>
+        <Link to="#">
+          <button>All Events</button>
+        </Link>
+      </div>
+      <div id="loading"></div>
+      <div className="events-container" id="events-container"></div>
+    </div>
+
+    <div className="about-us">
+      <h2 className="title">{title}</h2>
+      <p className="description">{description}</p>
+      <Link to="#">
+        <button>About</button>
+      </Link>    
+    </div>
+
+    <hr/>
+
+    <div className="music-container">
+      <div className="artist-container">
+        <h2 className="title">Artist Spotlight</h2>
+        <img src="img/truxtonmile.jpg" alt="artists" />
+        <h2 className="artist-name">Truxton Mile</h2>
+        <h4 className="description">Friday, July 19, 2019 @ Cellar Door</h4>
+        <Link to="#">
+          <button>Buy Tickets</button>
+        </Link>
+        <Link to="https://www.truxtonmile.com/">
+          <button>Website</button>
+        </Link>
+      </div>
+      <div className="spotify-container">
+        <iframe src="https://open.spotify.com/embed/artist/1o2oLRSKlPBVWKTQjfpNH5" width="100%" height="100%" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
+    </div>
+
+    <hr/>
+
+    <div className="food-and-drinks">
+      <div className="header">
+        <h3>Food & Drinks</h3>
+        <Link to="#">
+          <button>Menus</button>
+        </Link>
+      </div>
+      <p>{description}</p>      
+    </div>
+  </main>
 )
 
 IndexPageTemplate.propTypes = {
+  events_button_title: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string
 }
@@ -25,6 +79,7 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
+        events_button_title={frontmatter.events_button_title}
         title={frontmatter.title}
         description={frontmatter.description}
       />
@@ -46,6 +101,7 @@ export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
+        events_button_title
         title
         description
       }
