74392a59e7ab9ea0367c2e196d1d1b827178e161
amitp <amitpatel_7@msn.com>
2019-07-27 19:36:19 -0700

    implemented navbar code from Cellar Door repo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
-  }
-
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
-  }
-
-  render() {
-    return (
-      <header
-        className="nav is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <Link to="#" className="nav-item community">Community</Link>
-            <Link to="#" className="nav-item about-us">About Us</Link>
-            <Link to="#" className="cellar-door-icon"><img src="img/dooricon.png" alt="homepage" style={{width: '20px'}} /></Link>
-            <Link to="#" className="nav-item calendar">Calendar</Link>
-            <Link to="#" className="nav-item fd">Food & Drink</Link>
-          </div>
+const Navbar = () => {
+  return (
+    <header>
+      <div className="nav">
+        <label htmlFor="toggle">&#9776;</label>
+        <input type="checkbox" id="toggle" />
+        <div className="menu">
+          <Link to="#" className="nav-item community">Community</Link>
+          <Link to="#" className="nav-item about-us">About Us</Link>
+          <Link to="#" className="cellar-door-icon"><img src="img/dooricon.png" alt="door_icon" /></Link>
+          <Link to="#" className="nav-item calendar">Calendar</Link>
+          <Link to="#" className="nav-item fd">Food & Drink</Link>
         </div>
-      </header>
-    )
-  }
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
