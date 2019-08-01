7663e705341d4d9af5d071a1787258a8c763cbb7
amitp <amitpatel_7@msn.com>
2019-08-01 05:51:31 -0700

    created Food&Drink page

---
+++ src/pages/foodanddrink.md
@@ -0,0 +1,3 @@
+---
+templateKey: food&drink-page
+---
---
+++ src/templates/food&drink-page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const CalendarPage = () => {
+  return (
+    <Layout>
+      <main>
+        <iframe src="./menu.pdf" title="menu" width="100%" height="80%"></iframe>
+      </main>
+    </Layout>
+  )
+}
+
+export default CalendarPage
