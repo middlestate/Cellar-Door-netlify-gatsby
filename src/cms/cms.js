3801c3d291008f83ab0675c77cb74ba5f13ce8dd
amitp <amitpatel_7@msn.com>
2019-08-02 14:10:05 -0700

    imported and registered CalendarPagePreview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,9 +4,11 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import CalendarPagePreview from './preview-templates/CalendarPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('calendar', CalendarPagePreview)
