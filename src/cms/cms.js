1c1a3c8bb9ad4ac06a8f1922fec1d76bd15f821d
amitp <amitpatel_7@msn.com>
2019-08-02 13:56:52 -0700

    imported AboutPagePreview inside cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
