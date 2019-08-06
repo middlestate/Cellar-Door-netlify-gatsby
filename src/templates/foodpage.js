25e1d7694e31a8ae1573438ea99d54364f15ec12
amitp <amitpatel_7@msn.com>
2019-08-06 13:21:08 -0700

    rendered menu.pdf in foodpage

--- src/templates/foodpage.js
+++ src/templates/foodpage.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-const FoodPage = () => {
-  return (
-    <Layout>
-      <main>
-        <iframe src="menu.pdf" title="menu" width="100%" height="80%"></iframe>
-      </main>
-    </Layout>
-  )
-}
+import menu from './menu.pdf'
 
-export default FoodPage
+export const FoodPageTemplate = ({
+  pdf
+}) => (
+    <Layout>
+      <embed src={menu} title="menu" style={{width:"100%", height:1024}} />
+    </Layout>
+)
+
+export default FoodPageTemplate
