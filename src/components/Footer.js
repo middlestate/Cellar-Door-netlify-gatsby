3127c281f0cc7114a952c1f0d479d4bb945e488d
amitp <amitpatel_7@msn.com>
2019-08-08 14:03:38 -0700

    Added social media links to font awesome links in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,16 +17,24 @@ const Footer = class extends React.Component {
           </Link>
           <div className="social-media">
             <div className="social-icon-container">
-              <FontAwesomeIcon icon={['fab', 'facebook-f']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />
+              <a href="https://www.facebook.com/cellardoor101/" title="https://www.facebook.com/cellardoor101/" target="__blank">
+                <FontAwesomeIcon icon={['fab', 'facebook-f']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
+              </a>
             </div>
             <div className="social-icon-container">
-              <FontAwesomeIcon icon={['fab', 'twitter']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />
+              <a href="https://twitter.com/cellardoor101?lang=en" title="https://twitter.com/cellardoor101?lang=en" target="__blank">
+                <FontAwesomeIcon icon={['fab', 'twitter']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
+              </a>
             </div>
             <div className="social-icon-container">
-            <FontAwesomeIcon icon={['fab', 'youtube']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />
+              <a href="https://www.youtube.com/watch?v=1pqvY9Oh0tQ" title="https://www.youtube.com/watch?v=1pqvY9Oh0tQ" target="__blank">
+                <FontAwesomeIcon icon={['fab', 'youtube']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
+              </a>
             </div>
             <div className="social-icon-container">
-              <FontAwesomeIcon icon={['fab', 'instagram']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />
+              <a href="http://instagram.com/cellardoor559" title="http://instagram.com/cellardoor559" target="__blank">
+                <FontAwesomeIcon icon={['fab', 'instagram']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
+              </a>
             </div>
           </div>
         </div>
