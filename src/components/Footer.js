608dc7901db44ac4c13987454bffbf008be45cf6
amitp <amitpatel_7@msn.com>
2019-08-08 14:40:33 -0700

    removed title from anchor elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,22 +17,22 @@ const Footer = class extends React.Component {
           </Link>
           <div className="social-media">
             <div className="social-icon-container">
-              <a href="https://www.facebook.com/cellardoor101/" title="https://www.facebook.com/cellardoor101/" target="__blank">
+              <a href="https://www.facebook.com/cellardoor101/" target="__blank">
                 <FontAwesomeIcon icon={['fab', 'facebook-f']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
               </a>
             </div>
             <div className="social-icon-container">
-              <a href="https://twitter.com/cellardoor101?lang=en" title="https://twitter.com/cellardoor101?lang=en" target="__blank">
+              <a href="https://twitter.com/cellardoor101?lang=en" target="__blank">
                 <FontAwesomeIcon icon={['fab', 'twitter']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
               </a>
             </div>
             <div className="social-icon-container">
-              <a href="https://www.youtube.com/watch?v=1pqvY9Oh0tQ" title="https://www.youtube.com/watch?v=1pqvY9Oh0tQ" target="__blank">
+              <a href="https://www.youtube.com/watch?v=1pqvY9Oh0tQ" target="__blank">
                 <FontAwesomeIcon icon={['fab', 'youtube']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
               </a>
             </div>
             <div className="social-icon-container">
-              <a href="http://instagram.com/cellardoor559" title="http://instagram.com/cellardoor559" target="__blank">
+              <a href="http://instagram.com/cellardoor559" target="__blank">
                 <FontAwesomeIcon icon={['fab', 'instagram']} style={{color: "#ffffff", display: "inline-block", width: "100%", position: "relative", top: "8px" }} size="lg" />              
               </a>
             </div>
