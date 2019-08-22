3cfde009e6537336465ffaf19e2670f08084e086
amitp <amitpatel_7@msn.com>
2019-08-21 22:34:57 -0700

    tastemakers gallery attempt

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,15 +4,11 @@ import PropTypes from 'prop-types'
 const Gallery = ({ gridItems }) => {
   return (
     <div className="gallery-row">
-      {gridItems.map((gridItem, keys) => {
+      {gridItems.map(({ image }, keys) => {
         return (
           <div key={keys} className="gallery-column">
             <img
-              src={
-                !!gridItem.image.childImageSharp
-                  ? gridItem.image.childImageSharp.fluid.src
-                  : gridItem.image
-              }
+              src={image}
               alt="artist image"
             />
           </div>
@@ -23,13 +19,11 @@ const Gallery = ({ gridItems }) => {
 }
 
 Gallery.propTypes = {
-  gallery: PropTypes.shape({
-    images: PropTypes.arrayOf(
-      PropTypes.shape({
-        image: PropTypes.string
-      })
-    )
-  })
+  gridItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string
+    })
+  ),
 }
 
 export default Gallery
